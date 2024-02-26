83c5566d0d6952ebce437b43cf069b7c81b996df
nphumekh <nutchaya.phumekham@cern.ch>
2024-02-26 17:29:27 +0700

    use require 2 ways

---
+++ requiremodule.js
@@ -0,0 +1,12 @@
+// practice using require module
+
+/* require the entire module
+const exportmodule = require('./exportmodule.js');
+const myUnitCircle = exportmodule.circleArea(1);
+*/
+
+//require just a function in a specific module
+const { circleArea } = require('./exportmodule');
+const myUnitCircle = circleArea(1);
+
+console.log(`${myUnitCircle} is the area of my unit circle, utilizing the require/ export module`);
