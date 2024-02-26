ae2381721fb5598d714c395b4eaaf17e0447e5ae
nphumekh <nutchaya.phumekham@cern.ch>
2024-02-26 17:48:31 +0700

    updated module.js

--- requiremodule.js
+++ requiremodule.js
@@ -6,7 +6,7 @@ const myUnitCircle = exportmodule.circleArea(1);
 */
 
 //require just a function in a specific module
-const { circleArea } = require('./exportmodule');
+const { circleArea } = require('./module');
 const myUnitCircle = circleArea(1);
 
 console.log(`${myUnitCircle} is the area of my unit circle, utilizing the require/ export module`);
