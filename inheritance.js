8fb2ca74de1fb3c5976a20e098ae572f1113899b
nphumekh <nutchaya.phumekham@cern.ch>
2024-02-26 17:09:21 +0700

    static method

--- inheritance.js
+++ inheritance.js
@@ -3,26 +3,36 @@ class HospitalEmployee {
       this._name = name;
       this._remainingVacationDays = 20;
     }
-    
     get name() {
       return this._name;
     }
-    
     get remainingVacationDays() {
       return this._remainingVacationDays;
     }
-    
     takeVacationDays(daysOff) {
       this._remainingVacationDays -= daysOff;
     }
-  }
-  
-  class Nurse extends HospitalEmployee{
-    constructor(name, certifications){
-      super(name);
-      this._remainingVacationDays = 20;
-      this._certifications = certifications;
+    static generatePassword() {
+      const password = Math.floor(Math.random()*10000);
+      return password;
     }
   }
   
-  const nurseOlynyk = new Nurse("Olynyk", "['Trauma', 'Pediatrics']")
+  class Nurse extends HospitalEmployee {
+    constructor(name, certifications) {
+      super(name);
+      this._certifications = certifications;
+    } 
+    get certifications() {
+      return this._certifications;
+    }
+    addCertification(newCertification) {
+      this.certifications.push(newCertification);
+    }
+  }
+  
+  const nurseOlynyk = new Nurse('Olynyk', ['Trauma','Pediatrics']);
+  nurseOlynyk.takeVacationDays(5);
+  console.log(nurseOlynyk.remainingVacationDays);
+  nurseOlynyk.addCertification('Genetics');
+  console.log(nurseOlynyk.certifications);
