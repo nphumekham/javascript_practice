6bdc8b1e5000a1c2e8535ae395e2b092b91e1c08
nphumekh <nutchaya.phumekham@cern.ch>
2024-02-28 23:33:29 +0700

    add another promise function

--- module.js
+++ module.js
@@ -182,6 +182,31 @@ let cookTheBeans = (isSoftened) => {
   });
 }
 
+// This function returns true 50% of the time.
+let randomSuccess = () => {
+  let num = Math.random();
+  if (num < .5 ){
+    return true;
+  } else {
+    return false;
+  }
+ };
+ 
+ // This function returns a promise that resolves half of the time and rejects half of the time
+ let cookBeanSouffle = () => {
+  return new Promise((resolve, reject) => {
+    console.log('Fingers crossed... Putting the Bean Souffle in the oven');
+    setTimeout(()=>{
+      let success = randomSuccess();
+      if(success){
+        resolve('Bean Souffle');
+      } else {
+        reject('Dinner is ruined!');
+      }
+    }, 1000);
+  });
+ };
 
 module.exports = { circleArea, squareArea, checkInventory, processPayment, 
-  shipOrder, checkAvailability, brainstormDinner, shopForBeans, soakTheBeans, cookTheBeans};
+  shipOrder, checkAvailability, brainstormDinner, shopForBeans, soakTheBeans, cookTheBeans,
+  cookBeanSouffle};
