201a3a12f7dbd02602be021b9fa435fd13e0b3be
nphumekh <nutchaya.phumekham@cern.ch>
2024-02-28 19:59:54 +0700

    add PROMISE

--- module.js
+++ module.js
@@ -21,6 +21,28 @@ function changeToFunkyColor(domElement) {
   domElement.style.color = `rgb(${r}, ${g}, ${b})`;
 }
 
+//below is for promise practice
+const inventory = {
+  sunglasses: 1900,
+  pants: 1088,
+  bags: 1344
+};
+//this is a PROMISE that takes in 1param ORDER
+const checkInventory = (order) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let inStock = order.every(item => inventory[item[0]] >= item[1]);
+      if (inStock) {
+        resolve(`Thank you. Your order was successful.`);
+      } else {
+        reject(`We're sorry. Your order could not be completed because some items are sold out.`);
+      }
+    }, 1000);
+  })
+};
+
+module.exports = { checkInventory };
+
 module.exports.circleArea = circleArea;
 
 module.exports.squareArea = squareArea;
