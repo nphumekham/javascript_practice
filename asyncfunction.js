1d7d9f60a0f1398e2f81d2b333866bbf1766e256
nphumekh <nutchaya.phumekham@cern.ch>
2024-02-28 23:07:29 +0700

    use async + await instead of native promise

--- asyncfunction.js
+++ asyncfunction.js
@@ -1,39 +1,4 @@
-const fs = require('fs');
-const promisifiedReadfile = require('./promisifiedReadfile');
-      
-// Here we use fs.readfile() and callback functions:
-fs.readFile('./file.txt', 'utf-8', (err, data) => {
-  if (err) throw err;
-  let firstSentence = data;
-  fs.readFile('./file2.txt',  'utf-8', (err, data) => {
-    if (err) throw err;
-    let secondSentence = data;
-    console.log(firstSentence, secondSentence);
-  });
-});
-
-// Here we use native promises with our "promisified" version of readfile:
-let firstSentence;
-promisifiedReadfile('./file.txt', 'utf-8')
-  .then((data) => {
-    firstSentence = data;
-    return promisifiedReadfile('./file2.txt', 'utf-8');
-  })
-  .then((data) => {
-    let secondSentence = data;
-    console.log(firstSentence, secondSentence)
-  })
-  .catch((err) => {console.log(err)});
-
-// Here we use promisifiedReadfile() again but instead of using the native promise .then() syntax, we declare and invoke an async/await function:
-async function readFiles() {
-  let firstSentence = await promisifiedReadfile('./file.txt', 'utf-8');
-  let secondSentence = await promisifiedReadfile('./file2.txt', 'utf-8');
-  console.log(firstSentence, secondSentence);
-}
-
-readFiles();
-
+const {brainstormDinner} = require("./module.js");
 
 //the below withConstructor function is function that returns a PROMISE 
 //which can also be written like withAsync function
@@ -54,3 +19,11 @@ function withAsync(num){
         return 'not zero';
     }
 }
+
+//use the async + await on a promise 
+async function whatIsMyDinner(){
+    let meal = await brainstormDinner();
+    console.log(`my dinner is ${meal}`);
+}
+
+whatIsMyDinner();
