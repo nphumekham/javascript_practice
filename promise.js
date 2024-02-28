88d15e0fed1926cfc980c1d55213d792a0b8e9e3
nphumekh <nutchaya.phumekham@cern.ch>
2024-02-28 19:40:06 +0700

    assign Promise to a variable and log its value

--- promise.js
+++ promise.js
@@ -2,17 +2,23 @@ const inventory = {
     sunglasses: 1900,
     pants: 1088,
     bags: 1344
-  };
-  
-  // Write your code below:
-  const myExecutor = (resolve, reject) => {
-    if (inventory.sunglasses > 0){
-      resolve('Sunglasses order processed.');
+};
+
+// Write your code below:
+const myExecutor = (resolve, reject) => {
+    if (inventory.sunglasses > 0) {
+        resolve('Sunglasses order processed.');
     } else {
-      reject('That item is sold out.');
+        reject('That item is sold out.');
     }
-  };
-  
-  const orderSunglasses = () => {
+};
+
+//this is a function that returns a PROMISE 
+//this is how to create generic function thats take 0 param
+const orderSunglasses = () => {
     return new Promise(myExecutor);
-    };
+};
+
+const orderPromise = orderSunglasses();
+
+console.log(orderPromise);                      
