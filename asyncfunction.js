6aca62a29ba43776fd06cc816eb304a6729ab163
nphumekh <nutchaya.phumekham@cern.ch>
2024-03-01 23:47:24 +0700

    use promise.all + await

--- asyncfunction.js
+++ asyncfunction.js
@@ -71,3 +71,11 @@ async function hostDinnerParty(){
   
   serveDinner();
   
+  //use promise.all
+  async function serveDinnerAgain(){
+    const foodArray = await Promise.all([steamBroccoli(), cookRice(), bakeChicken(), cookBeans()]);
+  
+    console.log(`Dinner is served. We're having ${foodArray[0]}, ${foodArray[1]}, ${foodArray[2]}, and ${foodArray[3]}.`);
+  }
+  
+  serveDinnerAgain();
